0a6de940ed8e6423e8b1a9c5d157928e6ff47898
ABdelhay Zaadaddi <abdezaadaddi06@gmail.com>
2024-01-30 08:17:05 +0100

    start workig o task 0

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,12 @@
+// function that create binary treee node
+// parent is a pointer to the parent node of the node to create
+// value is the value to put in the new node
+// When created, a node does not have any child
+// function must return a pointer to the new node, or NULL on failure
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+    binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+
+    return (new_node)
+}
