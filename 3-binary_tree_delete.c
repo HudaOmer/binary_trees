f5854f10952e676165aa0ce6113f03345f98c2a5
ABdelhay Zaadaddi <abdezaadaddi06@gmail.com>
2024-01-30 11:01:13 +0100

    working on task 4

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -2,22 +2,22 @@
 
 /**
  * binary_tree_delete - Deletes an entire binary tree.
- * @tree: pointer to the root of the tree.
+ * @tree: Pointer to the root of the tree.
  *
- * Description: This function deletes all nodes in the binary tree.
+ * Description: This function recursively deletes all nodes in the binary tree
  * starting from the leaves and moving towards the root.
-*/
+ */
 
 void binary_tree_delete(binary_tree_t *tree)
 {
 
-    if (tree)
-    {
-        if (!tree)
-            return;
-        binary_tree_delete(tree->left);
-        binary_tree_delete(tree->right);
+	if (tree)
+	{
+		if (!tree)
+			return;
+		binary_tree_delete(tree->left);
+		binary_tree_delete(tree->right);
 
-        free(tree);
-    }
+		free(tree);
+	}
 }
--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,11 +1,16 @@
 #include "binary_trees.h"
 
-// function that checks if a node is a leaf
-// where node is a pointer to the node to check
-// function must return 1 if node is a leaf, otherwise 0
-// if node is null return 0
+/**
+ * binary_tree_is_leaf - Checks if a node is a leaf in a binary tree.
+ * Description: A leaf node is a node with no left or right child.
+ * @node: Pointer to the node to check.
+ * Return: 1 if the node is a leaf, 0 otherwise.
+ */
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
+    if (node && !node->left && !node->right)
+        return (1);
 
+    return (0);
 }
