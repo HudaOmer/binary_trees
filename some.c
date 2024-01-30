a0bf19fc0d0e39d4c179c00cdcfc1634f8b8357d
HudaOmer <hudaoe175@gmail.com>
2024-01-30 14:15:44 -0800

    Solve task 13

--- some.c
+++ some.c
@@ -10,8 +10,14 @@
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
+	size_t nodes = 0;
+
 	if (tree)
+	{
 		if (tree->left || tree->right)
-			 return(1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
-	return (0);
+			nodes++;
+		nodes += binary_tree_nodes(tree->left);
+		nodes += binary_tree_nodes(tree->right);
+	}
+	return (nodes);
 }
