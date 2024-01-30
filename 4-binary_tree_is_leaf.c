eca77d03ad2cec4d47705874a82ef729b38803b7
ABdelhay Zaadaddi <abdezaadaddi06@gmail.com>
2024-01-30 11:17:12 +0100

    betty fixed

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -9,8 +9,8 @@
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-    if (node && !node->left && !node->right)
-        return (1);
+	if (node && !node->left && !node->right)
+		return (1);
 
-    return (0);
+	return (0);
 }
