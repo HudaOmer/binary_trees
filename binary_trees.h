acfd49a2b16a0f26830fb0665466b4b54263299b
ABdelhay Zaadaddi <abdezaadaddi06@gmail.com>
2024-01-30 08:55:43 +0100

    working on task 0

--- binary_trees.h
+++ binary_trees.h
@@ -1,5 +1,5 @@
-#if not defined
-#define
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -9,15 +9,17 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-struct binary_tree_s
+typedef struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
-};
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
+} binary_tree_t;
 
-typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s avl_t;
+typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s heap_t;
 
 /* library */
 #include <stdio.h>
