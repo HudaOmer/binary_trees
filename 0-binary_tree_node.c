691778ace887ab3c94cf04a10f87b159e304e19c
ABdelhay Zaadaddi <abdezaadaddi06@gmail.com>
2024-01-30 08:45:06 +0100

    working on task 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -4,9 +4,12 @@
 // When created, a node does not have any child
 // function must return a pointer to the new node, or NULL on failure
 
+#include "binary_trees.h"
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
     binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+
     if (!new_node)
     {
         free(new_node);
@@ -27,6 +30,6 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
         else
             return (NULL);
     }
-    
+
     return (new_node);
 }
