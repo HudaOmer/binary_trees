8ced7298323c9a3abbc75b2a877698209ca5ec82
ABdelhay Zaadaddi <abdezaadaddi06@gmail.com>
2024-01-30 08:59:56 +0100

    working on task 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -8,28 +8,28 @@
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
-    if (!new_node)
-    {
-        free(new_node);
-        return (NULL);
-    }
+	if (!new_node)
+	{
+		free(new_node);
+		return (NULL);
+	}
 
-    new_node->n = value;
-    new_node->left = NULL;
-    new_node->right = NULL;
-    new_node->parent = parent;
+	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
+	new_node->parent = parent;
 
-    if (parent)
-    {
-        if (!parent->left)
-            parent->left = new_node;
-        else if (!parent->right)
-            parent->right = new_node;
-        else
-            return (NULL);
-    }
+	if (parent)
+	{
+		if (!parent->left)
+			parent->left = new_node;
+		else if (!parent->right)
+			parent->right = new_node;
+		else
+			return (NULL);
+	}
 
-    return (new_node);
+	return (new_node);
 }
