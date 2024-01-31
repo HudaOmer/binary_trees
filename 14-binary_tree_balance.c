bb11d87fda777f2d95fdba858106203e0127dd06
ABdelhay Zaadaddi <abdezaadaddi06@gmail.com>
2024-01-31 15:08:07 +0100

    working on task 14

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,10 @@
+// function that measures the balace factor of a binary tree
+// tree is a pointer to the root node of the tree to measure the balance factor
+// If tree is NULL, return 0
+
+#include "binary_trees.h"
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+
+}
