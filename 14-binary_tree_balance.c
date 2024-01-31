066232ff8e8fcc75df85da0af826fcb27179abd0
ABdelhay Zaadaddi <abdezaadaddi06@gmail.com>
2024-01-31 15:22:16 +0100

    working on task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -6,5 +6,29 @@
 
 int binary_tree_balance(const binary_tree_t *tree)
 {
+    int right_height = 0;
+    int left_height = 0;
+    
+    if (tree)
+    {
+        left_height = height(tree->left);
+        right_height = height(tree->right);
+    }
+    else
+    {
+        return (0);
+    }
+    return (left_height - right_height);
+}
 
+int height(const binary_tree_t * tree)
+{
+    if (tree)
+    {
+        int left_height = 1 + height(tree->left);
+        int right_height = 1 + height(tree->right);
+
+        return (left_height > right_height ? left_height : right_height);
+    }
+    return (0);
 }
