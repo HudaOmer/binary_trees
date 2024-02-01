06bf350ca58ce369b6922b90b352adfebda46ef6
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2024-02-01 23:55:49 +0300

    Update 111-bst_insert.c

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -3,46 +3,47 @@
 /**
  * bst_insert - a function that inserts a value into a BST
  * @tree: is a pointer to the binary search tree to modify
- * @value: is the value to insert into the tree
+ * @value: is the value to insert  into the tree
  *
- * Return: pointer to the created node, NULL otherwise
+ * Return: pointer to the  created node, NULL otherwise
  */
 bst_t *bst_insert(bst_t **tree, int value)
 {
-	bst_t *new_node = NULL, *parent = NULL;
+	bst_t *curr, *new;
 
 	if (tree)
 	{
-		parent = *tree;
-		if (*tree == NULL)
+		curr = *tree;
+
+		if (!curr)
 		{
-			new_node = binary_tree_node(*tree, value);
-			*tree = new_node;
+			new = binary_tree_node(curr, value);
+			if (new == NULL)
+				return (NULL);
+			return (*tree = new);
 		}
-		else
+
+		if (value < curr->n)
 		{
-			while (parent)
-			{
-				if ((parent->n > value) && (parent->left))
-					parent = parent->left;
-				else if ((parent->n < value) && (parent->right))
-					parent = parent->right;
-				else
-					break;
-			}
-			if (parent->n < value)
-			{
-				new_node = binary_tree_node(parent, value);
-				parent->right = new_node;
-			}
-			else if (parent->n > value)
-			{
-				new_node = binary_tree_node(parent, value);
-				parent->left = new_node;
-			}
+			if (curr->left)
+				return (bst_insert(&curr->left, value));
+
+			new = binary_tree_node(curr, value);
+			if (new == NULL)
+				return (NULL);
+			return (curr->left = new);
+		}
+
+		if (value > curr->n)
+		{
+			if (curr->right)
+				return (bst_insert(&curr->right, value));
+
+			new = binary_tree_node(curr, value);
+			if (!new)
+				return (NULL);
+			return (curr->right = new);
 		}
 	}
-
-
-	return (new_node);
+	return (NULL);
 }
