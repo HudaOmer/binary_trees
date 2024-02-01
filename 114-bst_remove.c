40cc1d316e494267f1f68c6ecbd82bd86bf7c155
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2024-02-01 23:42:15 +0200

    Update 114-bst_remove.c

--- 114-bst_remove.c
+++ 114-bst_remove.c
@@ -16,7 +16,7 @@ bst_t *min_root(bst_t *root)
  * @root: is a pointer to the root node of the tree where you will remove a node
  * @value: is the value to remove in the tree
  * Once located, the node containing a value equals to value must be removed and freed
- * If the node to be deleted has two children, it must be replaced with its first 
+ * If the node to be deleted has two children, it must be replaced with its first
  * n-order successor (not predecessor)
  * Return: a pointer to the new root node of the tree after removing the desired value
  */
