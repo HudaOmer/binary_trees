30b9dfe2803d25e20ad817a25a9a0b959891d988
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2024-02-01 18:59:08 +0300

    Update 102-binary_tree_is_complete.c

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -4,31 +4,37 @@
  * isFullTree - this function checks if a tree is full
  * @root: is a pointer root of the tree
  * Return: true if full, false in not
+ */
 bool isFullTree (struct Node* root)
 {
 	/* If empty tree */
 	if (!root)
-    return true;
+		return true;
 
 	/* If leaf node */
-  if (!(root->left) && !(root->right))
-    return true;
+	if (!(root->left) && !(root->right))
+		return true;
 
-	/* If both left and right are not NULL, and left & right subtrees */
-	/* are full */
-  if ((root->left) && (root->right))
-    return (isFullTree(root->left) && isFullTree(root->right));
+/* If both left and right are not NULL, and left & right subtrees */
+/* are full */
+	if ((root->left) && (root->right))
+		return (isFullTree(root->left) && isFullTree(root->right));
 
-	/* We reach here when none of the above if conditions work */
-  return false;
+/* We reach here when none of the above if conditions work */
+	return false;
 }
 
 /**
- * binary_tree_is_complete - 
-
+ * binary_tree_is_complete - a function that checks if a binary tree is complete
+ * @tree: is a pointer to the root node of the tree to check
+ * Return: 1 if complete, 0 otherwise
+	if tree is NULL, your function must return 0
+ */
 int binary_tree_is_complete(const binary_tree_t *tree)
 {
-  if (isFullTree(root))
-    return (1);
-  return (0);
+	binary_tree_t *root = (binary_tree_t *)tree;
+
+	if (isFullTree(root))
+		return (1);
+	return (0);
 }
