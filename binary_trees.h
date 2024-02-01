b30792137a6d91e4e6823e4d6eb4bb0da603a06e
HudaOmer <104313411+HudaOmer@users.noreply.github.com>
2024-02-01 23:38:51 +0300

    Update binary_trees.h

--- binary_trees.h
+++ binary_trees.h
@@ -26,6 +26,7 @@ typedef struct binary_tree_s heap_t;
 #include <stdlib.h>
 #include <stddef.h>
 #include <stdbool.h>
+#include <limits.h>
 
 /* function prototype */
 void binary_tree_print(const binary_tree_t *tree);
